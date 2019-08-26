2baa0469fa6fc1b605267e975ea027153566fd02
Spencer Curtis <spencercolecurtis@gmail.com>
2019-08-26 10:46:01 -0600

    Add StructsAndClasses playground

---
+++ StructsAndClasses.playground/Contents.swift
@@ -0,0 +1,38 @@
+import Foundation
+/*:
+ #### Contact
+ 
+ 1. Create a struct called Contact
+ 2. Add `name: String` and `phoneNumber: String` properties. Think about whether these should be constants or variables.
+ 3. Write an initializer that takes `name` and `phoneNumber` as arguments and initializes the corresponding properties using `self.` syntax
+ */
+
+
+
+
+
+/*:
+ #### ContactManager
+ 
+ 1. Create a class called ContactsManager
+ 2. It should contain a variable array property called `contacts` whose type is an array of `Contact` structs
+ 3. Add a method (function) called `createContact(name: String, phoneNumber: String) -> Contact`. It should:
+    - Initialize a new instance of Contact
+    - Add it to the contacts property array
+    - Return it
+ */
+
+
+
+
+
+/*:
+ #### Test Your Code
+ 
+ 1. Create an instance of `ContactsManager`, and create various contacts using the `createContact` method.
+ 2. Print out the `ContactsManager` instance's `contacts` array to make sure they were added correctly. To access the array, remember to use dot syntax on the `ContactManager` instance you created in the previous step.
+*/
+ 
+ 
+ 
+  
